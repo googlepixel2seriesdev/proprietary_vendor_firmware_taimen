adbc8f15eb5cc6ba207750b9ca31115a953ba971
Ionut Gherman <ghermanionut96@gmail.com>
2023-11-18 14:20:17 +0200

    Initial taimen firmware commit

---
+++ config.mk
@@ -0,0 +1,16 @@
+FIRMWARE_IMAGES := \
+    abl \
+    aes \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    hyp \
+    keymaster \
+    laf \
+    modem \
+    pmic \
+    rpm \
+    tz \
+    xbl
+
+AB_OTA_PARTITIONS += $(FIRMWARE_IMAGES)
